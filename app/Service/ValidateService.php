7a2b2bed135b114f695d9f35eb6660d3efe9c982
jack01 <jack@126.com>
2023-06-28 14:52:11 +0800

    数据验证

--- app/Service/ValidateService.php
+++ app/Service/ValidateService.php
@@ -20,9 +20,19 @@ class ValidateService
      */
     protected $validationFactory;
 
-    public function validate($data, $rule, $errcode = 43000)
+    /**
+     * 数据验证
+     * author:jack
+     * date:2023/6/28 14:50
+     * @param $data 验证的数据
+     * @param $rule 验证规则
+     * @param int $errcode 错误码
+     * @param bool $custom 是否自定义错误说明
+     * @return array
+     */
+    public function validate($data, $rule, $errcode = 43000, $custom = false)
     {
-        $message = [];
+        $message = $custom ? $this->messages() : [];
         $validator = $this->validationFactory->make($data, $rule, $message);
         if ($validator->fails()) {
             $errorMessage = $validator->errors()->first();
@@ -34,6 +44,21 @@ class ValidateService
         return [
             'errcode' => 0,
             'errmsg' => 'ok'
+        ];
+    }
+
+    /**
+     * 自定义错误说明
+     * author:jack(jimke127@126.com)
+     * date:2023/6/28 14:47
+     * @return string[]
+     */
+    public function messages()
+    {
+        return [
+            'required' => 'The :attribute field is required.',
+            'between' => 'The :attribute value :input is not between :min - :max.',
+            'in' => 'The :attribute must be one of the following types: :values',
         ];
     }
 }
